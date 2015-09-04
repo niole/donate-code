944f01266d79c870d19dbae59efe7868c1836497
Niole Nelson <niolenelson@gmail.com>
2015-09-04 14:09:41 -0400

    have user name in signup. app broken

--- client/createaccount.js
+++ client/createaccount.js
@@ -1,16 +1,7 @@
 Meteor.startup = function() {
-  Accounts.ui.config({
-   passwordSignupFields: "EMAIL_ONLY"
-  });
-
-  AccountsTemplates.addField({
-      _id: 'name',
-      type: 'text',
-      displayName: "Name",
-      func: function(value){return value !== 'Full Name';},
-      errStr: 'Only "Full Name" allowed!',
-  });
-
+//  Accounts.ui.config({
+//   passwordSignupFields: "EMAIL_ONLY"
+//  });
 };
 
 Tracker.autorun(function () {
