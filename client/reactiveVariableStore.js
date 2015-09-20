eb86a102717bfafb55da35d4ba3ebeaae86cd0a8
Niole Nelson <niolenelson@gmail.com>
2015-09-20 18:06:40 -0400

    adds redirect ability on logout for when page is not refreshed

--- client/reactiveVariableStore.js
+++ client/reactiveVariableStore.js
@@ -1,7 +1,5 @@
 Tracker.autorun(function () {
-//  if (Meteor.user()) {
-//    FlowRouter.go('/'+Meteor.userId());
-//  } else {
-//    FlowRouter.go('/');
-//  }
+  if (!Meteor.user()) {
+    FlowRouter.go('/');
+  }
 });
